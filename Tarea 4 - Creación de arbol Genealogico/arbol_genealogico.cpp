e13a28f1c2a21d09135f33363782c94245e333a4
Laxharoth <zeydenlaxharoth@gmail.com>
2022-07-27 20:20:40 -0500

    Create arbol_genealogico.cpp

---
+++ Tarea 4 - Creación de arbol Genealogico/arbol_genealogico.cpp
@@ -0,0 +1,35 @@
+#include "arbol_genealogico.hpp"
+
+bool IntegranteFamilia::operator==(IntegranteFamilia const& rhs){ return (*this).id == rhs.id;}
+bool IntegranteFamilia::operator!=(IntegranteFamilia const& rhs){ return (*this).id != rhs.id;}
+bool IntegranteFamilia::operator>=(IntegranteFamilia const& rhs){ return (*this).id >= rhs.id;}
+bool IntegranteFamilia::operator<=(IntegranteFamilia const& rhs){ return (*this).id <= rhs.id;}
+bool IntegranteFamilia::operator>( IntegranteFamilia const& rhs){ return (*this).id > rhs.id; }
+bool IntegranteFamilia::operator<( IntegranteFamilia const& rhs){ return (*this).id < rhs.id; }
+
+void soloDerecha( void (*callback)(TreeNode<IntegranteFamilia> *node) , TreeNode<IntegranteFamilia> *curr){
+    if(curr == nullptr) return;
+    if(curr->right != nullptr)
+        callback(curr->right);
+    soloDerecha(callback, curr->right);
+    soloDerecha(callback, curr->left);
+}
+void printMasculino(Tree<IntegranteFamilia> &tree){
+    soloDerecha([](TreeNode<IntegranteFamilia> *node){ 
+        printIntegranteFamilia(node->data);
+     }, tree.root);
+}
+void printTararabuelo(Tree<IntegranteFamilia> &tree){
+    tree.levelorder([](TreeNode<IntegranteFamilia> *node){
+        if(node->left == nullptr && node->right == nullptr){
+            printIntegranteFamilia(node->data);
+        }
+    });
+}
+void printIntegranteFamilia(const IntegranteFamilia& integrante){
+    printf("************************\n");
+    printf("* Nombre : %s\n", integrante.nombre);
+    printf("* Fecha Nacimiento: %s\n", integrante.fechaNacimiento);
+    printf("* Lugar Nacimiento: %s\n", integrante.lugarNacimiento);
+    printf("* * *\n");
+}
