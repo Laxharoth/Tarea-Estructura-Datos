d091d99e7df125a7c24ff036a6f495a811661354
Laxharoth <zeydenlaxharoth@gmail.com>
2022-07-28 12:09:02 -0500

    Replace cin with istream pointer
    Allows use file to feed the program

--- Tarea 4 - Creación de arbol Genealogico/main.cpp
+++ Tarea 4 - Creación de arbol Genealogico/main.cpp
@@ -4,22 +4,30 @@
 #include <iostream>
 #include <string>
 #include <cstdio>
+#include <fstream>
+#include <memory>
 
 using std::cin;
 using std::string;
 
 const int WRONG_CHOICE_ERROR = 0b1;
 
-
+static std::istream *STREAM = &cin;
 void get_choice(int &choose, int min, int max);
 void get_string(string &str);
 void main_menu(Tree<IntegranteFamilia> &tree);
 void enterPause();
 void get_IntegranteFamilia(TreeNode<IntegranteFamilia> *node);
 
-int main(){
-    // int insert[15]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    // int insert[15]{15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+int main(int argc, char* argv[]){
+    std::unique_ptr<std::ifstream> file;
+    if(argc > 1){
+        const char* filename =  argv[1];
+        file = std::make_unique<std::ifstream>(filename);
+        if(file->is_open())
+            STREAM = file.get();
+    }
+
     int insert[15]{
         8,
         4,12,
@@ -40,15 +48,22 @@ int main(){
     // tree.levelorder( [](TreeNode<IntegranteFamilia> *node){ std::cout << node->data.nombre << std::endl; }) ;
     main_menu(tree);
 }
+void set_STREAM(){
+    if( STREAM != &cin && STREAM->peek() == EOF){
+        STREAM = &cin;
+    }
+}
 void get_choice(int &choose, int min, int max){
-    cin >> choose;
+    *STREAM >> choose;
     string rest;
-    std::getline(cin, rest);
+    std::getline(*STREAM, rest);
+    set_STREAM();
     if(choose < min || choose > max) throw WRONG_CHOICE_ERROR;
     system("cls");
 }
 void get_string(string &str){
-    std::getline(cin, str);
+    std::getline(*STREAM, str);
+    set_STREAM();
 }
 void main_menu(Tree<IntegranteFamilia> &tree){
     int opcion;
@@ -91,6 +106,6 @@ void get_IntegranteFamilia(TreeNode<IntegranteFamilia> *nodo){
 }
 void enterPause(){
     printf("\nPresina [Enter] para continuar:");
-    getchar();
+    if( STREAM ==  &cin) getchar();
     system("cls");
 }
