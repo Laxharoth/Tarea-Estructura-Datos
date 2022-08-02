519df7133d319d853bf1c930b4daa7520691f056
Laxharoth <zeydenlaxharoth@gmail.com>
2022-08-02 12:30:56 -0500

    Created structure

---
+++ Tarea 5 - Rastreo de pedidos/PaquetePedido.cpp
@@ -0,0 +1,18 @@
+#include "PaquetePedido.hpp"
+
+bool PaquetePedido::operator<=(const PaquetePedido &other) const { return this->numero_pedido <= other.numero_pedido; }
+bool PaquetePedido::operator>=(const PaquetePedido &other) const { return this->numero_pedido >= other.numero_pedido; }
+bool PaquetePedido::operator<( const PaquetePedido &other) const { return this->numero_pedido <  other.numero_pedido; }
+bool PaquetePedido::operator>( const PaquetePedido &other) const { return this->numero_pedido >  other.numero_pedido; }
+bool PaquetePedido::operator==(const PaquetePedido &other) const { return this->numero_pedido == other.numero_pedido; }
+bool PaquetePedido::operator!=(const PaquetePedido &other) const { return this->numero_pedido != other.numero_pedido; }
+
+PaquetePedido::PaquetePedido():numero_pedido(++next_numero_pedido){}
+PaquetePedido::PaquetePedido( string nombre_remitente, string nomre_destinatario, string estado_envío, string destino_direccion ):
+    numero_pedido(++next_numero_pedido), 
+    nombre_remitente(nombre_remitente),nomre_destinatario(nomre_destinatario), 
+    estado_envío(estado_envío),destino_direccion(destino_direccion){}
+PaquetePedido::PaquetePedido(const PaquetePedido& other):
+    numero_pedido(other.numero_pedido), 
+    nombre_remitente(nombre_remitente),nomre_destinatario(nomre_destinatario),
+    estado_envío(estado_envío),destino_direccion(destino_direccion){}
---
+++ Tarea 5 - Rastreo de pedidos/PaquetePedido.hpp
@@ -0,0 +1,31 @@
+#ifndef PAQUETE_PEDIDO_HPP
+#define PAQUETE_PEDIDO_HPP
+
+#include <string>
+using std::string;
+
+static long long next_numero_pedido = 0;
+
+struct PaquetePedido{
+    string nombre_remitente;
+    string nomre_destinatario;
+    string estado_envío;
+    string destino_direccion;
+    PaquetePedido();
+    PaquetePedido(
+        string nombre_remitente,
+        string nomre_destinatario,
+        string estado_envío,
+        string destino_direccion
+    );
+    PaquetePedido(const PaquetePedido& other);
+    bool operator<=(const PaquetePedido &other) const;
+    bool operator>=(const PaquetePedido &other) const;
+    bool operator<(const PaquetePedido &other) const;
+    bool operator>(const PaquetePedido &other) const;
+    bool operator==(const PaquetePedido &other) const;
+    bool operator!=(const PaquetePedido &other) const;
+    const long long numero_pedido;
+};
+
+#endif
