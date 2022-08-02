5df6101f172a483c65737102a9ae1ab4ace8a92e
Laxharoth <zeydenlaxharoth@gmail.com>
2022-08-02 12:37:00 -0500

    Update main.cpp
    clean up main

--- Tarea 5 - Rastreo de pedidos/main.cpp
+++ Tarea 5 - Rastreo de pedidos/main.cpp
@@ -1,42 +1,7 @@
 #include "b-tree.hpp"
-
-#include <iostream>
-
-void printBNode(BNode *node);
-
-void printBTree(BTree &tree){
-    printBNode(tree.root);
-}
-
-static int IIII = 0;
-
-void printBNode(BNode *node){
-    if(node == nullptr)return;
-    for (int i = 0; i < node->inserted; i++){
-        printBNode(node->children[i]);
-        std::cout<< IIII++ << ":" << node->keys[i] << std::endl;
-    }
-    printBNode(node->children[node->inserted]);
-}
-
+#include "PaquetePedido.hpp"
 
 int main(){
-    BTree tree;
-
-    for (int i = 0; i < 50; i++){
-        tree.insert(i);
-    }
-    try{
-        for (int i = 0; i < 50; i++){
-            findBKey(i, tree.root);
-        }
-    }catch(int ex){
-        std::cout << ex << std::endl;
-    }
-
-    // printBTree(tree);
-
-    std::cout << "Hello world!" << std::endl;
-
+    BTree<int> tree;
     return 0; 
 }
